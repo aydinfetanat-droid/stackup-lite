9422888a068c144bd1474af353a7d418122dbfa9
STACKUP LEGEND2000 <aydin.fetanat@gmail.com>
2026-09-21 01:38:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="StackUp Lite", page_icon="💰"
+st.set_page_config(page_title="StackUp Lite", page_icon="💰")
 page = st.sidebar.radio ("Go to", ["Home","About"])
 if page == home:
   st.title("StackUp Lite") 
